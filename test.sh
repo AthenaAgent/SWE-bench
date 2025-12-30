a99bff97fe9e6fda5740eb4e55ef4681588d2cd4
soumik12345 <19soumik.rakshit96@gmail.com>
2025-12-30 22:14:17 +0530

    update: evals for ansible

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 clear
-python -m swebench.harness.run_evaluation \
+uv run python -m swebench.harness.run_evaluation \
     --dataset_name AthenaAgent42/ansible-swe-bench-pro \
     --split test \
     --predictions_path preds.json \
     --max_workers 4 \
-    --run_id my_evaluation \
-    --namespace 'preds'
+    --run_id sample_evaluation \
+    --namespace none \
+    --force_rebuild true
     
